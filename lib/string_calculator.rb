1177059e0cc48d6ff75f70905172fa52d945d682
David Andrews <david@ryatta.com>
2015-01-28 20:29:40 -0500

    Results after requirement 8.

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,7 +1,17 @@
 module StringCalculator
 
   def self.add(string)
-    0
+    delimiter, string = extract_delimiter(string)
+    string.split(delimiter).inject(0) { |sum, x| sum + x.to_i }
+  end
+
+  def self.extract_delimiter(string)
+    delimiter = /[,\n]/
+    if string =~ /^\/\//
+      delimiter = string[2]
+      string = string[4..-1]
+    end
+    [ delimiter, string ]
   end
 
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -11,6 +11,49 @@ RSpec.describe StringCalculator, "#add" do
     expect(StringCalculator.add("")).to eq(0)
   end
 
+  it "returns 0 for the string '0'" do
+    expect(StringCalculator.add("0")).to eq(0)
+  end
+
+  it "returns 5 for the string '5'" do
+    expect(StringCalculator.add("5")).to eq(5)
+  end
+ 
+  it "returns 3 for the string '1,2'" do
+    expect(StringCalculator.add("1,2")).to eq(1+2)
+  end
+
+  it "returns 16 for the string '7,9'" do
+    expect(StringCalculator.add("7,9")).to eq(7+9)
+  end
+
+  it "returns 57 for the string '12,45'" do
+    expect(StringCalculator.add("12,45")).to eq(57)
+  end
+
+  it "returns 201 for the string '42,159'" do
+    expect(StringCalculator.add("42,159")).to eq(201)
+  end
+
+  it "returns 4002 for the string '1558,2,2442'" do
+    expect(StringCalculator.add("1558,2,2442")).to eq(4002)
+  end
+
+  it "returns 161 for the string '15,22,45,79'" do
+    expect(StringCalculator.add("15,22,45,79")).to eq(161)
+  end
+
+  it "returns 6 for the string '1\n2,3'" do
+    expect(StringCalculator.add("1\n2,3")).to eq(6)
+  end
+
+  it "returns 7 for the string '//;\n2;5'" do
+    expect(StringCalculator.add("//;\n2;5")).to eq(7)
+  end
+
+  it "throws exception when the string contains a negative number" do
+    expect { StringCalculator.add("-1") }.to raise_error
+  end
   # more tests go here
 end
 
