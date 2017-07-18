7bac72bc55fdd9255c79fb778f723629c5106dad
David Andrews <david@ryatta.com>
2017-07-18 10:42:59 -0400

    WIP by the Workshop night team - up to requirement 8

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,7 +1,24 @@
 module StringCalculator
 
   def self.add(string)
-    # code to be tested goes here...
+    delim, string = extract_delim(string)
+    string.gsub!(/\n/, delim)
+    return to_i_with_raise(string) unless string.include?delim 
+    digit_strings = string.split(delim)
+    digit_strings.inject(0) { |t,s| t + to_i_with_raise(s) }
+  end
+
+  def self.extract_delim(string)
+    delim = ","
+    return delim, string unless string[0..1] == "//"
+    delim = string[2]
+    [ delim, string[4..-1] ]
+  end
+
+  def self.to_i_with_raise(string)
+    i = string.to_i
+    raise "No negatives!" if i < 0
+    i
   end
 
   # ...and here when the above becomes too complex.
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -11,5 +11,39 @@ RSpec.describe StringCalculator, "#add" do
     expect(StringCalculator.add("")).to eql(0)
   end
 
+  it "returns the value for a single digit" do
+    (0..9).each do |digit|
+      expect(StringCalculator.add(digit.to_s)).to eql(digit)
+    end
+  end
+
+  it "returns the sum of two digits when separated by a comma" do
+    expect(StringCalculator.add("1,2")).to eql(3)
+    expect(StringCalculator.add("7,9")).to eql(16)
+  end
+
+  it "returns the sum of two strings of digits when separated by a comma" do
+    expect(StringCalculator.add("12,45")).to eql(57)
+    expect(StringCalculator.add("42,159")).to eql(201)
+  end
+
+  it "returns the sum of multiple strings of digits when separatd by commas" do
+    expect(StringCalculator.add("1558,2,2442")).to eql(4002)
+    expect(StringCalculator.add("15,22,45,79")).to eql(161)
+  end
+
+  it "handles newlines like they were commas" do
+    expect(StringCalculator.add("1\n2,3")).to eql(6)
+  end
+
+  it "allows the delimiter to be set" do
+    expect(StringCalculator.add("//;\n2;5")).to eql(7)
+  end
+ 
+  it "raises an error if it finds a nagtive number" do
+    expect { StringCalculator.add("2,5,-6") }.to raise_error
+    expect { StringCalculator.add("1\n-2,5") }.to raise_error
+    expect { StringCalculator.add("//;\n2;-5;3") }.to raise_error
+  end
   # more tests go here
 end
